483292b42523116d6446578ce889e19956a46ea4
bderudder <bjorn.derudder@gmail.com>
2015-04-29 16:19:39 +0200

    Updated Database links

--- db/cms_project_links.sql
+++ db/cms_project_links.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.1
+-- version 4.0.4.2
 -- http://www.phpmyadmin.net
 --
--- Host: 10.246.16.118:3306
--- Generation Time: Apr 29, 2015 at 01:22 PM
--- Server version: 5.5.42-MariaDB-1~wheezy
--- PHP Version: 5.3.3-7+squeeze15
+-- Machine: localhost
+-- Genereertijd: 29 apr 2015 om 14:18
+-- Serverversie: 5.6.13
+-- PHP-versie: 5.4.17
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `bdmultimedia_be`
+-- Databank: `test`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cms_project_links`
+-- Tabelstructuur voor tabel `cms_project_links`
 --
 
 CREATE TABLE IF NOT EXISTS `cms_project_links` (
@@ -35,13 +35,13 @@ CREATE TABLE IF NOT EXISTS `cms_project_links` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `cms_project_links`
+-- Gegevens worden uitgevoerd voor tabel `cms_project_links`
 --
 
 INSERT INTO `cms_project_links` (`inhoud`, `link`, `show`, `login`, `admin`) VALUES
 ('Home', 'index.php', 1, 0, 0),
 ('About', 'about.php', 1, 0, 0),
-('Admin', 'admin.php', 1, 2, 1),
+('Admin', 'backend', 1, 2, 1),
 ('Login', 'login.php', 1, 1, 0),
 ('Personal', 'personal.php', 1, 2, 0),
 ('How To', 'howto.php', 1, 0, 0);
